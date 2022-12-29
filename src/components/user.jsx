c14cd49d177c21438773fba3a13a793e86f98351
SberIrk <114170642+SberIrk@users.noreply.github.com>
2022-12-29 21:46:20 +0800

    Ok

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -6,6 +6,7 @@ const Users=()=>{
 
     const [storeUsers, setStoreUsers] = useState(api.users.fetchAll());
 
+    // Зарисовка таблицы
     const renderPhrase  = () => {
 
         const handleButtonClick = (userID) => {
@@ -14,7 +15,7 @@ const Users=()=>{
             });
         }
 
-        const renderQulities  = (dataQualityUser) => {
+        const renderQualities  = (dataQualityUser) => {
             if(!Array.isArray(dataQualityUser) && dataQualityUser.length < 1){
                 return;
             }
@@ -40,7 +41,7 @@ const Users=()=>{
                     storeUsers.map( user => (
                         <tr key={user._id}>
                             <td>{user.name}</td>
-                            <td>{renderQulities (user.qualities)} </td>
+                            <td>{renderQualities (user.qualities)} </td>
                             <td>{(user?.profession?.name) ? user.profession.name : ""}</td>
                             <td>{user.completedMeetings}</td>
                             <td>{`${user.rate} / 5`}</td>
@@ -52,11 +53,12 @@ const Users=()=>{
         );
     }
 
-
+    // Пустая строка данных
     if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
         return (<h2><span className="badge bg-danger m-2">Никто с тобой не тусанет</span></h2>);
     }
 
+    // Есть данные, заполняем таблицу...
     const count = storeUsers.length;
     const msg = `${count} ${declOfNum(count, ['человек', 'человека', 'человек'])} тусанут с тобой сегодня.`
     return (<>
