9906806ff0a339dda7e79dcc7074f8669a77c304
SberIrk <114170642+SberIrk@users.noreply.github.com>
2022-12-29 21:41:37 +0800

    Ok

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -6,49 +6,24 @@ const Users=()=>{
 
     const [storeUsers, setStoreUsers] = useState(api.users.fetchAll());
 
-    const renderedHeaderTable = () => {
-        if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
-            return (<h2><span className="badge bg-danger m-2">Никто с тобой не тусанет</span></h2>);
+    const renderPhrase  = () => {
+
+        const handleButtonClick = (userID) => {
+            setStoreUsers((store)=> {
+                return store.filter(value => value._id !== userID);
+            });
         }
-        const count = storeUsers.length;
-        const msg = `${count} ${declOfNum(count, ['человек', 'человека', 'человек'])} тусанут с тобой сегодня.`
-        return (<h2><span className="badge bg-primary m-2">{msg}</span></h2>);
 
-    };
-
-    const buttonDeleteUser = (userID) => {
-        setStoreUsers((store)=> {
-            return store.filter(value => value._id !== userID);
-        });
-    }
-
-    const qualitiesRendered = (dataQualityUser) => {
-        if(!Array.isArray(dataQualityUser) && dataQualityUser.length < 1){
-            return;
+        const renderQulities  = (dataQualityUser) => {
+            if(!Array.isArray(dataQualityUser) && dataQualityUser.length < 1){
+                return;
+            }
+            return dataQualityUser.map(quality => {
+                const color = `m-2 badge bg-${quality.color}`
+                return (<span key = {quality._id} className = {color} >{quality.name}</span>)
+            });
         }
-        return dataQualityUser.map(quality => {
-            const color = `m-2 badge bg-${quality.color}`
-            return (<span key = {quality._id} className = {color} >{quality.name}</span>)
-        });
-    }
 
-    const rowRender = (user) => {
-        return (
-            <tr key={user._id}>
-                <td>{user.name}</td>
-                <td>{qualitiesRendered(user.qualities)} </td>
-                <td>{(user?.profession?.name) ? user.profession.name : ""}</td>
-                <td>{user.completedMeetings}</td>
-                <td>{`${user.rate} / 5`}</td>
-                <td><button type="button" className="btn btn-danger" onClick={() => buttonDeleteUser(user._id)}>Удалить</button></td>
-            </tr>
-        );
-    }
-
-    const renderedBodyTable = () => {
-        if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
-            return
-        }
         return (
             <table className="table">
                 <thead>
@@ -61,23 +36,35 @@ const Users=()=>{
                     <th scope="col"></th>
                 </tr>
                 </thead>
-                <tbody>
-                {storeUsers.map( dataUser => rowRender(dataUser))}
-                </tbody>
+                <tbody>{
+                    storeUsers.map( user => (
+                        <tr key={user._id}>
+                            <td>{user.name}</td>
+                            <td>{renderQulities (user.qualities)} </td>
+                            <td>{(user?.profession?.name) ? user.profession.name : ""}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{`${user.rate} / 5`}</td>
+                            <td><button type="button" className="btn btn-danger" onClick={() => handleButtonClick(user._id)}>Удалить</button></td>
+                        </tr>
+                    )
+                )}</tbody>
             </table>
         );
     }
 
-    const usersRendered = () => {
-        return(
-            <>
-                {renderedHeaderTable()}
-                {renderedBodyTable()}
-            </>
-        );
+
+    if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
+        return (<h2><span className="badge bg-danger m-2">Никто с тобой не тусанет</span></h2>);
     }
 
-    return usersRendered()
+    const count = storeUsers.length;
+    const msg = `${count} ${declOfNum(count, ['человек', 'человека', 'человек'])} тусанут с тобой сегодня.`
+    return (<>
+             <h2><span className="badge bg-primary m-2">{msg}</span></h2>
+                {renderPhrase()}
+            </>
+    );
+
 }
 
 export default Users;
