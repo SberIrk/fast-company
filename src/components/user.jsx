a7757948b12bb88625e5ee5b12c308ea074938f9
SberIrk <114170642+SberIrk@users.noreply.github.com>
2022-12-29 21:11:19 +0800

    Ok

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -2,33 +2,37 @@ import React , {useState} from "react";
 import api from "../api"
 import {declOfNum} from "../utils"
 
-const dataUsers = api.users.fetchAll().reduce((calc,dataUser) =>
-    Object.assign(calc,{[dataUser._id] : dataUser}),{});
-
-
 const Users=()=>{
 
-    const [storeUsers, setStoreUsers] = useState(dataUsers);
-    const [countUsers, setCountUsers] = useState(Object.keys(storeUsers).length);
+    const [storeUsers, setStoreUsers] = useState(api.users.fetchAll());
 
-    const _buttonDeleteUser = (userID) => {
+    const renderedHeaderTable = () => {
+        if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
+            return (<h2><span className="badge bg-danger m-2">Никто с тобой не тусанет</span></h2>);
+        }
+        const count = storeUsers.length;
+        const msg = `${count} ${declOfNum(count, ['человек', 'человека', 'человек'])} тусанут с тобой сегодня.`
+        return (<h2><span className="badge bg-primary m-2">{msg}</span></h2>);
+
+    };
+
+    const buttonDeleteUser = (userID) => {
         setStoreUsers((store)=> {
-            delete store[userID];
-            setCountUsers(Object.keys(store).length);
-            return store;
+            return store.filter(value => value._id !== userID);
         });
     }
 
-    const _rowRender = (user) => {
-        const qualitiesRendered = (dataQualityUser) => {
-            if(Array.isArray(dataQualityUser) && dataQualityUser.length === 0){
-                return;
-            }
-            return dataQualityUser.map(quality => {
-                const color = `m-2 badge bg-${quality.color}`
-                return (<span key = {quality._id} className = {color} >{quality.name}</span>)
-            });
+    const qualitiesRendered = (dataQualityUser) => {
+        if(!Array.isArray(dataQualityUser) && dataQualityUser.length < 1){
+            return;
         }
+        return dataQualityUser.map(quality => {
+            const color = `m-2 badge bg-${quality.color}`
+            return (<span key = {quality._id} className = {color} >{quality.name}</span>)
+        });
+    }
+
+    const rowRender = (user) => {
         return (
             <tr key={user._id}>
                 <td>{user.name}</td>
@@ -36,21 +40,13 @@ const Users=()=>{
                 <td>{(user?.profession?.name) ? user.profession.name : ""}</td>
                 <td>{user.completedMeetings}</td>
                 <td>{`${user.rate} / 5`}</td>
-                <td><button type="button" className="btn btn-danger" onClick={() => _buttonDeleteUser(user._id)}>Удалить</button></td>
+                <td><button type="button" className="btn btn-danger" onClick={() => buttonDeleteUser(user._id)}>Удалить</button></td>
             </tr>
         );
     }
 
-    const _headerTable = (count) => {
-        if(count) {
-            const msg = `${count} ${declOfNum(count, ['человек', 'человека', 'человек'])} тусанут с тобой сегодня.`
-            return (<h2><span className="badge bg-primary m-2">{msg}</span></h2>);
-        }
-        return (<h2><span className="badge bg-danger m-2">Никто с тобой не тусанет</span></h2>);
-    };
-
-    const _renderTable = (count) => {
-        if(count < 1) {
+    const renderedBodyTable = () => {
+        if(!Array.isArray(storeUsers) || storeUsers.length < 1) {
             return
         }
         return (
@@ -66,18 +62,22 @@ const Users=()=>{
                 </tr>
                 </thead>
                 <tbody>
-                {Object.values(storeUsers).map(dataUser => _rowRender(dataUser))}
+                {storeUsers.map( dataUser => rowRender(dataUser))}
                 </tbody>
             </table>
         );
     }
 
-    return(
-        <>
-            {_headerTable(countUsers)}
-            {_renderTable(countUsers)}
-        </>
-    );
+    const usersRendered = () => {
+        return(
+            <>
+                {renderedHeaderTable()}
+                {renderedBodyTable()}
+            </>
+        );
+    }
+
+    return usersRendered()
 }
 
 export default Users;
