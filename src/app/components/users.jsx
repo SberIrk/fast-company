f46d98d9d3d250da9a97fa9706260843e5b1eda5
SberIrk <114170642+SberIrk@users.noreply.github.com>
2023-01-07 02:25:45 +0800

    update 0.14

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -3,22 +3,28 @@ import User from "./user"
 
 const Users=({users,...rest})=>{
     return (
-        <table className="table">
-            <thead>
-            <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Избраное</th>
-                <th scope="col"></th>
-            </tr>
-            </thead>
-            <tbody>{
-                users.map( user => (<User {...rest} {...user} key={user._id} />)
-            )}</tbody>
-        </table>
+        <>
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Профессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избраное</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map( user => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </>
     );
 
 }
