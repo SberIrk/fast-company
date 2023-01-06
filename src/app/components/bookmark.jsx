207f873fc7d65b003e5744b0072a954aeaf38ece
SberIrk <114170642+SberIrk@users.noreply.github.com>
2023-01-07 01:21:50 +0800

    update 0.11

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -4,7 +4,8 @@ const Bookmark  = (props) => {
     const {handleBookmark,user} = props;
     const classButton = (user.bookmark) ? "bi bi-toggle-on" : "bi bi-toggle-off";
     return (
-        <button className={classButton} onClick = { () => handleBookmark(user._id) }> </button> )
+        <button className={classButton} onClick = { () => handleBookmark(user._id) }> </button>
+    );
 }
 
 export default Bookmark;
